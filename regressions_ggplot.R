cad2704b136b3ad8f1003a186077651fa8c1d179
matthewacarson <github-carsonm@snkmail.com>
2023-11-29 13:39:55 -0800

    11/29

--- regressions_ggplot.R
+++ regressions_ggplot.R
@@ -1,4 +1,4 @@
-# Run data wrangling file first ####
+# Run data wrangling files first ####
 source(file = "limited_serv_wrangling.R", local = wrangled <- new.env())
 source(file = "all_industries_data_wrangling.R", local = wrangled)
 
@@ -35,7 +35,7 @@ felm_proportion <-
   felm(proportion_limited ~ treat * post | area_title,
        data = data_2012_2013)
 
-summary(felm_all)
+summary(felm_proportion)
 
 felm_all <-
   felm(emplvl_limited ~ treat * post + emplvl_all | area_title,
@@ -47,7 +47,7 @@ felm_cont_treatment <-
   felm(emplvl_limited ~ min_wage + emplvl_all | area_title + date, 
        data = joined$data)
 
-summary(models$felm)
+summary(felm_cont_treatment)
 
 
 # try using this with felm:  xactDOF = TRUE)
@@ -58,36 +58,38 @@ summary(models$felm)
 deci_period_prop_chg <- seq(2011.5, 2022.5, by = 1)
 
 joined$data |> 
-  filter(year_decimal %in% deci_period_prop_chg & State == "KS") |> 
-  aggregate(proportion_limited ~ State + year_decimal, FUN = mean) |> 
-  arrange(State) |> 
+  filter(year_decimal %in% deci_period_prop_chg & state == "KS") |> 
+  aggregate(proportion_limited ~ state + year_decimal, FUN = mean) |> 
+  arrange(state) |> 
   mutate(change = c(NA, diff(proportion_limited))) |> 
   add_row(
     joined$data |> 
-      filter(year_decimal %in% deci_period_prop_chg & State == "MO") |> 
-      aggregate(proportion_limited ~ State + year_decimal, FUN = mean) |> 
-      arrange(State) |> 
+      filter(year_decimal %in% deci_period_prop_chg & state == "MO") |> 
+      aggregate(proportion_limited ~ state + year_decimal, FUN = mean) |> 
+      arrange(state) |> 
       mutate(change = c(NA, diff(proportion_limited)))) |> 
   pivot_wider(
-    names_from = State,
+    names_from = state,
     values_from = c('proportion_limited', 'change')) #|> 
 # write_csv(file = "prop_chg.csv")
 # select(-proportion_limited) |> 
 # pivot_wider(
-#   names_from = State,
+#   names_from = state,
 #   values_from = change
 # )
-ggsave(filename = "means_with_loess.png", dpi = 'retina')
+# ggsave(filename = "means_with_loess.png", dpi = 'retina')
 # Calculate means ####
+# 
+# select which years youa re interested in:
 deci_period_means_trend <- seq(2011, 2012 + 11/12, by = 1/12)
 
 # Checking parallel trends assumption
 
 means_per_period <- joined$data |> 
   filter(year_decimal %in% deci_period_means_trend) |> 
-  aggregate(proportion_limited ~ year_decimal + State, FUN = mean) |> 
+  aggregate(proportion_limited ~ year_decimal + state, FUN = mean) |> 
   pivot_wider(
-    names_from = State,
+    names_from = state,
     values_from = proportion_limited
   ) |> data.frame()
 diff_KS <- diff(means_per_period$KS)
@@ -101,7 +103,7 @@ plot(
   pch = 16
 )
 abline(lm_diffs, col = 'red')
-plot(lm_diffs)
+# plot(lm_diffs)
 
 write_csv(x = means_per_period, file = "means_per_period.csv")
 
@@ -109,11 +111,11 @@ cor(means_per_period$KS, means_per_period$MO)
 
 # scatter plot ####
 ggplot(data = joined$data |> filter(year_decimal %in% deci_period_means_trend), 
-       aes(x = year_decimal, y = proportion_limited, color = State)) +
+       aes(x = year_decimal, y = proportion_limited, color = state)) +
   # geom_point(position = position_dodge(width = 0.02)) +
   geom_smooth(method = "loess", se = FALSE, span = 0.3) +
   geom_line(
-    aes(group = State),
+    aes(group = state),
     stat = "summary",
     fun = mean,
     lwd = 1) +
@@ -131,22 +133,36 @@ ggplot(data = joined$data |> filter(year_decimal %in% deci_period_means_trend),
 
 # Plotting means to check for parallel trends
 ggplot(data = joined$data |> filter(year_decimal %in% deci_period_means_trend), 
-       aes(x = year_decimal, y = proportion_limited, color = State)) +
+       aes(x = year_decimal, y = proportion_limited, color = state)) +
   geom_smooth(method = "loess", se = FALSE, span = 0.3, lwd = 0.75) +
   stat_summary(
     fun = mean,
+    size = 2,
     geom = "point") +
   geom_line(
-    aes(group = State),
+    aes(group = state),
     stat = "summary",
+    lwd = 0.7,
     fun = mean) +
   # scale_x_continuous(
   # breaks = seq(2016, 2020, by = 0.25),
   # limits = c(2011, 2023.25)
   # ) +
-  labs(title = "Means Plot") + 
-  scale_y_continuous(breaks = seq(0.035, 0.05, by = 0.0025)) +
+  labs(
+    title = "Mean Per Month",
+    subtitle = "Mean proportion within each county of total employed in limited service restaurants",
+    caption = "*Loess line with span of 0.3",
+    x = "Year",
+    y = "Mean Proportion",
+    color = "State*"
+    ) +
+  scale_y_continuous(breaks = seq(0.025, 0.05, by = 0.0025)) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+  theme(
+    legend.position = "right",
+    # plot.caption = element_text(
+      # hjust = 1.15, vjust = 0.7),# margin = margin(t = 10, b = 10)),
+    axis.text.x = element_text(
+      angle = 90, vjust = 0.5, hjust = 1))
 
-ggsave(filename = "means_plot_16_19.png", dpi = 'retina')
+ggsave(filename = "means_plot_11_12_no_loess.png", dpi = 'retina')
