b25901289991315e5f4cfa154167877c10fb402c
matthewacarson <github-carsonm@snkmail.com>
2023-12-02 16:18:00 -0800

    12/2 4:15

---
+++ average_weekly_wage.R
@@ -0,0 +1,39 @@
+# Code from Matthew Marsh
+
+wrangled$limited_combined$combined$state <- str_detect(wrangled$limited_combined$combined$area_title, pattern = "Missouri") |> ifelse(test = _,yes = "MO",no =  "KS")
+change_avg_wkly_wg <- wrangled$limited_combined$combined |>
+  aggregate(avg_wkly_wage ~ state + year, FUN = mean)# |>
+  pivot_wider(
+    names_from = year,
+    values_from = avg_wkly_wage
+  )
+
+change_avg_wkly_wg
+
+ggplot(
+  data = change_avg_wkly_wg,
+  aes(
+    x = year,
+    y = avg_wkly_wage,
+    fill = state)) + 
+  geom_bar(
+  stat = "identity", 
+  position = position_dodge(width = 0.85), 
+  color = 'black', 
+  # linewidth = 0.01,
+  width = 0.75) + 
+  scale_x_continuous(breaks = 2011:2023) +
+  labs(
+    title = "Average Weekly Wage",
+    x = "Year",
+    y = "US Dollars"
+  ) + 
+  guides(fill=guide_legend(title="State")) +
+  theme_light()
+
+ggsave(
+  filename = "average_weekly_wage.png",
+  dpi = 'retina'
+)
+  
+
